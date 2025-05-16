0253df9f31b70574ad16e86c02a46669a91a4408
Nisha Jha <nishajha1515@gmail.com>
2025-05-16 23:04:54 +0530

    Update

--- app/auth/page.jsx
+++ app/auth/page.jsx
@@ -8,6 +8,8 @@ import React, { useEffect, useState } from 'react'
 function Login() {
 
       const router = useRouter();
+      const isLocalhost = typeof window !== 'undefined' && window.location.hostname === 'localhost';
+
       
 
  useEffect(() => {
